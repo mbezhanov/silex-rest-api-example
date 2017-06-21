a81ddca108d541ace3cfb34f42f921c816442eec
Marin Bezhanov <marin.bezhanov@gmail.com>
2017-06-21 14:32:02 +0300

    configure Doctrine

--- src/app.php
+++ src/app.php
@@ -1,7 +1,28 @@
 <?php
 
+use Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
 use Silex\Application;
+use Silex\Provider\DoctrineServiceProvider;
 
 $app = new Application();
+
+$app->register(new DoctrineServiceProvider, [
+    'db.options' => [
+        'driver' => 'pdo_sqlite',
+        'path' => __DIR__ . '/sqlite.db',
+    ],
+]);
+
+$app->register(new DoctrineOrmServiceProvider, [
+    'orm.em.options' => [
+        'mappings' => [
+            [
+                'type' => 'annotation',
+                'namespace' => 'App\Entities',
+                'path' => __DIR__ . '/Entities',
+            ],
+        ],
+    ],
+]);
 
 return $app;
--- src/console.php
+++ src/console.php
@@ -4,12 +4,29 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 set_time_limit(0);
 
+use Doctrine\DBAL\Tools\Console\ConsoleRunner as DoctrineDBAL;
+use Doctrine\ORM\Tools\Console\ConsoleRunner as DoctrineORM;
+use Kurl\Silex\Provider\DoctrineMigrationsProvider;
 use Symfony\Component\Console\Application;
 
 $app = require __DIR__.'/../src/app.php';
 
 $console = new Application('REST API', '0.1.0');
-
 $console->setDispatcher($app['dispatcher']);
+
+$app->register(
+    new DoctrineMigrationsProvider($console),
+    [
+        'migrations.directory' => __DIR__ . '/../migrations',
+        'migrations.namespace' => 'App\Migrations',
+    ]
+);
+$app->boot();
+
+$helperSet = $console->getHelperSet();
+$helperSet->set($helperSet->get('connection'), 'db');
+
+DoctrineDBAL::addCommands($console);
+DoctrineORM::addCommands($console);
 
 $console->run();
